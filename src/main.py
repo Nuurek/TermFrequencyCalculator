d9409a9c970e5bef0b42456d7988018bd0e49a1c
JakubEasySolar <1j8MEh5yrDQ@%WN*>
2018-10-17 14:05:22 +0200

    Added query input and searching through documents.

--- src/main.py
+++ src/main.py
@@ -11,18 +11,16 @@ TERMS_PATH = RESOURCES_PATH / 'keywords.txt'
 if __name__ == '__main__':
     terms_loader = TermsLoader(TERMS_PATH)
     terms = terms_loader.load()
-    print(terms)
 
     documents_loader = DocumentsLoader(DOCUMENTS_PATH)
     documents = documents_loader.load()
-    print([document.content for document in documents])
 
     calculator = TermFrequencyCalculator(terms, documents)
 
-    print(calculator._terms)
-    print([document.tokens for document in calculator._documents])
-    print([document.bag_of_words for document in calculator._documents])
-    print([document.term_frequencies for document in calculator._documents])
-    print(calculator._inverse_document_frequencies)
-    print([document.inverse_term_frequencies for document in calculator._documents])
-    print([document.vector_length for document in calculator._documents])
+    while True:
+        query = input()
+        results = calculator.search(query)
+
+        for result in results:
+            print(f'{result.similarity}\t{result.document.title}')
+        print('\n')
--- src/term_frequency_calculator.py
+++ src/term_frequency_calculator.py
@@ -7,6 +7,19 @@ from document import Document
 from tokenizer import Tokenizer
 
 
+class SearchResult:
+
+    def __init__(self, similarity: float, document: Document):
+        self.similarity = similarity
+        self.document = document
+
+    def __gt__(self, other):
+        return self.similarity > other.similarity
+
+    def __lt__(self, other):
+        return self.similarity < other.similarity
+
+
 class TermFrequencyCalculator:
     _stemmer = PorterStemmer()
     _tokenizer = Tokenizer()
@@ -20,28 +33,55 @@ class TermFrequencyCalculator:
 
         self._documents: List[Document] = documents
         self._prepare_documents()
+
+        self._calculate_documents_bag_of_words()
         self._calculate_numbers_of_documents_containing_term()
         self._remove_unused_terms()
         self._calculate_documents_term_frequencies()
 
         self._calculate_terms_inverse_document_frequencies()
         self._calculate_documents_inverse_term_frequencies()
-        self._calculate_documents_vector_lengths()
+        self._calculate_documents_vector_length()
+
+    def search(self, query) -> List[SearchResult]:
+        query_document = Document(query, query)
+        self._prepare_document(query_document)
+        self._calculate_document_bag_of_words(query_document)
+        self._calculate_document_term_frequencies(query_document)
+        self._calculate_document_inverse_term_frequencies(query_document)
+        self._calculate_document_vector_length(query_document)
+
+        results = []
+        for document in self._documents:
+            similarity = self._calculate_documents_similarity(document, query_document)
+            results.append(SearchResult(similarity, document))
+
+        results.sort(reverse=True)
+
+        return results
 
     def _prepare_terms(self):
         self._terms = set([self._stemmer.stem(term) for term in self._terms])
 
     def _prepare_documents(self):
         for document in self._documents:
-            document.tokens = self._tokenizer.tokenize(document.content)
-            document.tokens = [self._stemmer.stem(token) for token in document.tokens]
+            self._prepare_document(document)
 
-            document.bag_of_words = {term: 0 for term in self._terms}
-            for token in document.tokens:
-                if token in self._terms:
-                    document.bag_of_words[token] += 1
+    def _prepare_document(self, document: Document):
+        document.tokens = self._tokenizer.tokenize(document.content)
+        document.tokens = [self._stemmer.stem(token) for token in document.tokens]
 
-            document.maximum_value_of_bag_of_words = max(document.bag_of_words.values())
+    def _calculate_documents_bag_of_words(self):
+        for document in self._documents:
+            self._calculate_document_bag_of_words(document)
+
+    def _calculate_document_bag_of_words(self, document: Document):
+        document.bag_of_words = {term: 0 for term in self._terms}
+        for token in document.tokens:
+            if token in self._terms:
+                document.bag_of_words[token] += 1
+
+        document.maximum_value_of_bag_of_words = max(document.bag_of_words.values())
 
     def _calculate_numbers_of_documents_containing_term(self):
         self._numbers_of_documents_containing_term = {
@@ -60,9 +100,12 @@ class TermFrequencyCalculator:
 
     def _calculate_documents_term_frequencies(self):
         for document in self._documents:
-            document.term_frequencies = {
-                term: document.bag_of_words[term] / document.maximum_value_of_bag_of_words for term in self._terms
-            }
+            self._calculate_document_term_frequencies(document)
+
+    def _calculate_document_term_frequencies(self, document: Document):
+        document.term_frequencies = {
+            term: document.bag_of_words[term] / document.maximum_value_of_bag_of_words for term in self._terms
+        }
 
     def _calculate_terms_inverse_document_frequencies(self):
         self._inverse_document_frequencies = {
@@ -80,8 +123,26 @@ class TermFrequencyCalculator:
                 term: document.term_frequencies[term] * self._inverse_document_frequencies[term] for term in self._terms
             }
 
-    def _calculate_documents_vector_lengths(self):
+    def _calculate_document_inverse_term_frequencies(self, document: Document):
+        document.inverse_term_frequencies = {
+            term: document.term_frequencies[term] * self._inverse_document_frequencies[term] for term in self._terms
+        }
+
+    def _calculate_documents_vector_length(self):
         for document in self._documents:
-            document.vector_length = sqrt(sum([
-                pow(frequency, 2) for frequency in document.inverse_term_frequencies.values()
-            ]))
+            self._calculate_document_vector_length(document)
+
+    @staticmethod
+    def _calculate_document_vector_length(document: Document):
+        document.vector_length = sqrt(sum([
+            pow(frequency, 2) for frequency in document.inverse_term_frequencies.values()
+        ]))
+
+    def _calculate_documents_similarity(self, first: Document, second: Document):
+        numerator = 0
+        for term in self._terms:
+            numerator += first.inverse_term_frequencies[term] * second.inverse_term_frequencies[term]
+
+        denominator = first.vector_length * second.vector_length
+
+        return numerator / denominator
